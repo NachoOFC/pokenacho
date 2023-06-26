f0ec59fa00be3ace1290b233815b9d7926b8e96a
SebastianIHS <134305630+SebastianIHS@users.noreply.github.com>
2023-06-26 18:55:16 -0400

    wawaw
    aaaa

--- app/Http/Controllers/Api/ConfiguracionController.php
+++ app/Http/Controllers/Api/ConfiguracionController.php
@@ -1,47 +1,114 @@
-namespace App\Http\Controllers;
+<?php
+
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
 
-class ConfiguracionController extends Controller
+class AuthController extends Controller
 {
-    public function cambiarContrasena(Request $request)
-    {
-        // Validar los datos enviados en la solicitud
-        $request->validate([
-            'password_actual' => 'required',
-            'password_nueva' => 'required|min:8|confirmed',
-        ]);
 
-        // Obtener el usuario autenticado
-        $user = Auth::user();
+ public function cambiarContrasena(Request $request)
+ {
+    // Validar los datos enviados en la solicitud
+    $request->validate([
+        'nombre' => 'required',
+        'correo' => 'required|email',
+        'password_actual' => 'required',
+        'password_nueva' => 'required|min:8|confirmed',
+    ]);
 
-        // Verificar si la contraseña actual coincide con la proporcionada
-        if (!password_verify($request->input('password_actual'), $user->password)) {
-            return redirect()->back()->withErrors(['password_actual' => 'La contraseña actual es incorrecta.']);
-        }
+    // Obtener el usuario autenticado
+    $user = Auth::user();
 
-        // Actualizar la contraseña del usuario
-        $user->password = bcrypt($request->input('password_nueva'));
-        $user->save();
-
-        return redirect()->back()->with('success', 'Contraseña cambiada correctamente.');
+    // Verificar si el nombre, correo y contraseña actual coinciden con los proporcionados
+    if ($request->input('nombre') !== $user->name) {
+        return redirect()->back()->withErrors(['nombre' => 'El nombre no coincide.']);
     }
 
-    public function cambiarDescripcion(Request $request)
-    {
-        // Validar los datos enviados en la solicitud
-        $request->validate([
-            'descripcion' => 'required',
-        ]);
-
-        // Obtener el usuario autenticado
-        $user = Auth::user();
-
-        // Actualizar la descripción del usuario
-        $user->descripcion = $request->input('descripcion');
-        $user->save();
-
-        return redirect()->back()->with('success', 'Descripción cambiada correctamente.');
+    if ($request->input('correo') !== $user->email) {
+        return redirect()->back()->withErrors(['correo' => 'El correo no coincide.']);
     }
+
+    if (!password_verify($request->input('password_actual'), $user->password)) {
+        return redirect()->back()->withErrors(['password_actual' => 'La contraseña actual es incorrecta.']);
+    }
+
+    // Actualizar la contraseña del usuario
+    $user->password = bcrypt($request->input('password_nueva'));
+    $user->save();
+
+    return redirect()->back()->with('success', 'Contraseña cambiada correctamente.');
+}
+
+public function cambiarDescripcion(Request $request)
+{
+    // Validar los datos enviados en la solicitud
+    $request->validate([
+        'descripcion' => 'required',
+    ]);
+
+    // Obtener el usuario autenticado
+    $user = Auth::user();
+
+    // Actualizar la descripción del usuario
+    $user->descripcion = $request->input('descripcion');
+    $user->save();
+
+    return redirect()->back()->with('success', 'Descripción cambiada correctamente.');
+}
+public function cambiarContrasena(Request $request)
+{
+    // Validar los datos enviados en la solicitud
+    $request->validate([
+        'nombre' => 'required',
+        'correo' => 'required|email',
+        'password_actual' => 'required',
+        'password_nueva' => 'required|min:8|confirmed',
+    ]);
+
+    // Obtener el usuario autenticado
+    $user = Auth::user();
+
+    // Verificar si el nombre, correo y contraseña actual coinciden con los proporcionados
+    if ($request->input('nombre') !== $user->name) {
+        return redirect()->back()->withErrors(['nombre' => 'El nombre no coincide.']);
+    }
+
+    if ($request->input('correo') !== $user->email) {
+        return redirect()->back()->withErrors(['correo' => 'El correo no coincide.']);
+    }
+
+    if (!password_verify($request->input('password_actual'), $user->password)) {
+        return redirect()->back()->withErrors(['password_actual' => 'La contraseña actual es incorrecta.']);
+    }
+
+    // Actualizar la contraseña del usuario
+    $user->password = bcrypt($request->input('password_nueva'));
+    $user->save();
+
+    return redirect()->back()->with('success', 'Contraseña cambiada correctamente.');
+}
+
+public function cambiarDescripcion(Request $request)
+{
+    // Validar los datos enviados en la solicitud
+    $request->validate([
+        'descripcion' => 'required',
+    ]);
+
+    // Obtener el usuario autenticado
+    $user = Auth::user();
+
+    // Actualizar la descripción del usuario
+    $user->descripcion = $request->input('descripcion');
+    $user->save();
+
+    return redirect()->back()->with('success', 'Descripción cambiada correctamente.');
+}
 }
